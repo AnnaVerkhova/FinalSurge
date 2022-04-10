2b4749987791ec769c0ccf6feaab1afc0ef1516e
Tanya Gladkova <tanya.gladkova.2013@mail.ru>
2022-04-10 12:21:59 +0300

    commit config

--- src/test/java/utils/CapabilitiesGenerator.java
+++ src/test/java/utils/CapabilitiesGenerator.java
@@ -6,13 +6,13 @@ public class CapabilitiesGenerator {
 
     public static ChromeOptions getChromeOptions() {
         ChromeOptions options = new ChromeOptions();
-        String driverPath = "src/test/resources/webdrivers/mac/chromedriver";
+        String driverPath = "src/test/resources/webdrivers";
         String os = System.getProperty("os.name").toLowerCase();
         System.out.println("Operational system: " + os + "; Driver path: " + driverPath);
         if (os.contains("win")) {
             System.setProperty("webdriver.chrome.driver", driverPath + "/Windows/chromedriver.exe");
-        } else if (os.contains("mac")) {
-            System.setProperty("webdriver.chrome.driver", driverPath);
+        } else if (os.contains("webdrivers/mac")) {
+            System.setProperty("webdriver.chrome.driver", driverPath + "chromedriver");
         }
         options.addArguments("--ignore-certificate-errors");
         options.addArguments("--disable-popup-blocking");
