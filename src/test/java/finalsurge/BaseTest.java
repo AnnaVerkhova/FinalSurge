5b34cd8d814d4e339625829d5ee8d12c5d5ff1ed
Anna <werxowaanna1991@gmail.com>
2022-04-10 17:56:33 +0300

    Add utils and logoutTest

--- src/test/java/finalsurge/BaseTest.java
+++ src/test/java/finalsurge/BaseTest.java
@@ -1,5 +1,6 @@
 package finalsurge;
 
+import org.openqa.selenium.Dimension;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.testng.annotations.AfterMethod;
@@ -19,6 +20,7 @@ public class BaseTest {
     public void setup() {
         driver = new ChromeDriver(CapabilitiesGenerator.getChromeOptions());
         driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(15));
+        driver.manage().window().setSize(new Dimension(1920, 1080));
         mainStep = new MainStep(driver);
     }
 
